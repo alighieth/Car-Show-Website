5412776ecc74d727c18a01fc6ebc0b56dea8a628
alighieth <alighieth2709@gmail.com>
2023-07-28 17:48:25 +0300

    initiated the code

---
+++ car_showcase_website/utils/index.ts
@@ -0,0 +1,45 @@
+export async function fetchCars() {
+  const headers = {
+    "X-RapidAPI-Key": "752305d5edmsh5a7fc6d75848f99p1f27b3jsnaf3a7b428003",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera`,
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const updateSearchParams = (type: string, value: string) => {
+  // Get the current URL search params
+  const searchParams = new URLSearchParams(window.location.search);
+
+  // Set the specified search parameter to the given value
+  searchParams.set(type, value);
+
+  // Set the specified search parameter to the given value
+  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+  return newPathname;
+};
